2673a6baaf8a1b9838c56898f24b5863568730bd
Allan Korir <kipkorir@princeton.edu>
2020-03-31 21:57:07 -0400

    basic connectivity

---
+++ profsDB.py
@@ -0,0 +1,102 @@
+from os import path
+from sys import argv, stderr
+from sqlite3 import connect
+from prof import Professor
+
+class profsDB:
+
+    def __init__(self):
+        self.conn = None
+
+    def connect(self):
+        DATABASE_NAME = 'profs.sqlite'
+        error_statement = ''
+
+        if not path.isfile(DATABASE_NAME):
+            error_statement = 'database profs.sqlite not found'
+            print(error_statement, file=stderr)
+        else:
+            self.conn = connect(DATABASE_NAME)
+
+        return error_statement
+
+    def disconnect(self):
+        self.conn.close()
+
+    def displayAllProfessors(self, cursor):
+        stmtStr = 'SELECT profs.netid, profs.title, profs.first, profs.last, profs.email,' + \
+                ' profs.phone, profs.website, profs.rooms, profs.department, profs.area,' + \
+                ' profs.bio' + \
+                ' FROM profs ' + \
+                ' ORDER BY profs.last ASC'
+        cursor.execute(stmtStr)
+        return self.return_profs(cursor)
+
+    def displayProfessorsByFilter(self, cursor, search_criteria, input_arguments):
+        stmtStr = 'SELECT profs.netid, profs.title, profs.first, profs.last, profs.email,' \
+                ' profs.phone, profs.website, profs.rooms, profs.department, profs.area,' \
+                ' profs.bio' + \
+                ' FROM profs' + \
+                ' WHERE ' + search_criteria + \
+                ' ORDER BY profs.last ASC'
+        print(stmtStr)
+        print(input_arguments)
+        cursor.execute(stmtStr, input_arguments)
+        return self.return_profs(cursor)
+
+    def return_profs(self, cursor): 
+        profs = []
+        row = cursor.fetchone()
+        while row is not None:
+            prof = Professor(row[0])
+            prof.setName(row[1], row[2], row[3])
+            prof.setEmail(row[4])
+            prof.setPhoneNumber(row[5])
+            prof.setWebsite(row[6])
+            prof.setRooms(row[7])
+            prof.setDepartment(row[8])
+            prof.setResearchAreas(row[9])
+            prof.setBio(row[10])
+            profs.append(prof)
+            row = cursor.fetchone()
+        return profs
+
+    def return_profs_list(self, profs):
+        profs_list = []
+        for prof in profs:
+            prof_listing = []
+            prof_listing.append(prof.getNetId())
+            prof_listing.append(prof.getName())
+            prof_listing.append(prof.getEmail())
+            prof_listing.append(prof.getPhoneNumber())
+            prof_listing.append(prof.getWebsite())
+            rooms = " ".join(prof.getRooms())
+            prof_listing.append(rooms)
+            prof_listing.append(prof.getDepartment())
+            researchAreas = " ".join(prof.getResearchAreas())
+            prof_listing.append(researchAreas)
+            prof_listing.append(prof.getBio())
+            profs_list.append(prof_listing)
+        return profs_list
+
+    def print_profs(self, profs):
+        profs_list = self.return_profs_list(profs)
+        for prof in profs_list:
+            prof_ = ''
+            for item in prof:
+                prof_ +=  ' ' + item
+            print(prof_) 
+
+
+if __name__ == '__main__':
+    profsDB = profsDB()
+    error_statement = profsDB.connect()
+    if error_statement == '':
+        cursor = profsDB.conn.cursor()
+        profs = profsDB.displayAllProfessors(cursor)
+        profsDB.print_profs(profs)
+        profsDB.disconnect()
+    else:
+        print(error_statement)
+        
+
---
+++ runserver.py
@@ -0,0 +1,88 @@
+from sys import argv, stderr
+from flask import Flask, request, redirect, make_response, url_for
+from flask import render_template
+from prof import Professor
+from search import profsDB
+
+app = Flask(__name__, template_folder='.')
+
+def getProfs(search_criteria, input_arguments):
+    profsDB_ = profsDB()
+    error_statement = profsDB_.connect()
+    listOfProfs = []
+    if error_statement == '':
+        cursor = profsDB_.conn.cursor()
+        if len(input_arguments) != 0:
+            profs = profsDB_.displayProfessorsByFilter(cursor, search_criteria, input_arguments)
+        else:
+            profs = profsDB_.displayAllProfessors(cursor)
+        profs = profsDB_.return_profs_list(profs)
+        profsDB_.disconnect()
+    else:
+        print(error_statement)
+    return profs, error_statement
+
+@app.route('/')
+def index():
+    html = render_template('index.html')
+    response = make_response(html)
+    return response
+
+@app.route('/search')
+def search():
+    html = render_template('search.html')
+    response = make_response(html)
+    return response
+
+@app.route('/profs')
+def profs():   
+    search_criteria, input_arguments = getSearchCriteria()
+
+    profs, error_statement = getProfs(search_criteria, input_arguments)
+
+    if error_statement == '':
+        html = \
+            render_template('profs.html', profs=profs)
+    else:
+        html = render_template('profs.html', error_statement=error_statement)
+        print(error_statement, file=stderr)
+    response = make_response(html)
+    return response
+
+def getSearchCriteria():
+    input_arguments = []
+
+    name = request.args.get('name')
+    area = request.args.get('area')
+    netid = request.args.get('netid')
+
+    search_criteria = ''
+    if name != '':
+        search_criteria += '' + 'first' + ' LIKE ' + '?' + ' OR '
+        search_criteria += '' + 'last' + ' LIKE ' + '?' + ' AND '
+        input_arguments.append('%'+name+'%')
+        input_arguments.append('%'+name+'%')
+
+    if area != '':
+        search_criteria += 'area' + ' LIKE ' + '?' + ' AND '
+        input_arguments.append('%'+area+'%')
+    if netid != '':
+        search_criteria += 'netid' + ' LIKE ' + '?' + ' AND '
+        input_arguments.append('%'+netid+'%')
+
+    search_criteria = search_criteria[:-5]
+    return search_criteria, input_arguments
+
+if __name__ == '__main__':
+    
+    if (len(argv) != 2):
+        print('Usage: ' + argv[0] + ' port', file=stderr)
+        exit(1)
+
+    try:
+        port = int(argv[1])
+    except:
+        print("Port must be an integer", file=stderr)
+        exit(1) 
+
+    app.run(host='0.0.0.0', port=int(argv[1]), debug=True)
