5b3e8dfe04f5129d31f2b9995b2f9d79f82c6cf1
Allan Korir <kipkorir@princeton.edu>
2020-04-17 21:24:11 -0400

    removed bottom margin when panel is collapsed

--- static/js/collapse.js
+++ static/js/collapse.js
@@ -3,9 +3,11 @@ function collapse(id) {
     let img = document.getElementById("img-" + id)
     if (panel.style.maxHeight){
         panel.style.maxHeight = null;
+        panel.style.marginBottom = null;
         img.src = "static/plus.png"
     } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
+        panel.style.marginBottom = "1%";
         img.src = "static/minus.png"
     }
 }
