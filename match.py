06762456374e476a279a47b49503b159a01b810b
blhuynh1999 <57556053+blhuynh1999@users.noreply.github.com>
2020-05-05 22:25:59 -0500

    Removed duplicate asterisks, fixed cost function so putting same prof multiple times does not increase likelihood of getting that prof

--- match.py
+++ match.py
@@ -69,14 +69,17 @@ def optimizePreferences(student_cap, pref_limit):
     for prefs in studentprefs:
         student_id = prefs.pop(0)
         cost = pref_limit - 1
+        pref_duplicates = []
         for pref in reversed(prefs):
-            if pref not in profids:
-                if pref != "Non ORFE professor":
-                    profids.append(pref)
-                else:
-                    student_id += "*"
-            costs[pref, student_id] = cost
-            cost -= 1
+            if pref not in pref_duplicates:
+                if pref not in profids:
+                    if pref != "Non ORFE professor":
+                        profids.append(pref)
+                    else:
+                        student_id += "*"
+                costs[pref, student_id] = cost
+                cost -= 1
+                pref_duplicates.append(pref)
         studentids.append(student_id)
 
     for prof in profids:
